c66cf6988f83fb07f28c4ab6692e5cddd4851e8e
ITO Koichi <koic.ito@gmail.com>
2012-03-30 18:15:28 +0900

    Version bump to 0.3.0

--- death.c
+++ death.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     int i;
     for (i = 0; i < 10; i++) {
        system("say -v Ralph deeeeeeeeeeeattttth &");
+       system(strcat("kill ", argv));
     }
     return 0;
 }

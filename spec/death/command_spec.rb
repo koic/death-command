427fa9f45ccee38552a449ca6cdc2096e3e25e65
Koichi ITO <koic.ito@gmail.com>
2015-06-23 00:02:01 +0900

    :lipstick:

--- spec/death/command_spec.rb
+++ spec/death/command_spec.rb
@@ -1,9 +1,9 @@
 describe Death::Command do
   describe '.death' do
-    include_context 'defined never end process class'
+    include_context 'defined never ending process class'
 
     before do
-      @pid = fork { NeverEndProcess.new.run }
+      @pid = fork { NeverEndingProcess.new.run }
     end
 
     specify { expect { Death::Command.death('-KILL', @pid) }.not_to raise_error }
--- spec/death/killer_spec.rb
+++ spec/death/killer_spec.rb
@@ -1,9 +1,9 @@
 describe Death::Killer do
   describe '.kill' do
-    include_context 'defined never end process class'
+    include_context 'defined never ending process class'
 
     before do
-      @pid = fork { NeverEndProcess.new.run }
+      @pid = fork { NeverEndingProcess.new.run }
     end
 
     specify { expect { Death::Killer.kill('-KILL', @pid) }.not_to raise_error }
